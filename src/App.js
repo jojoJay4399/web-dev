f6428ecf02d987632c42fd3b93b76c8c90703a15
LANGemmmmm <jiedong_lang@outlook.com>
2022-03-20 01:15:39 -0400

    a6 React.js sp22 again 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import './vendors/bootstrap/css/bootstrap.min.css';
+// import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 import HelloWorld from "./components/hello-world";
